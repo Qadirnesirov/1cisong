115b4763c5cd68a42fea7c436f2f12e1f8bc1a65
qadirnesirov02 <123129882+Qadirnesirov@users.noreply.github.com>
2024-03-09 22:38:53 +0400

    update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config(object):
-    DATABASE = os.environ.get("DB_URI")
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5011359168").split())
-    SUPPORT = os.environ.get("SUPPORT")
+    DATABASE = os.environ.get("DB_URI", "mongodb+srv://nesirovq1997:qadir1997@cluster0.pavador.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6184936428").split())
+    SUPPORT = os.environ.get("SUPPORT", "otobotsport")
